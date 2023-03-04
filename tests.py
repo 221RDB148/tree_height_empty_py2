642f4fc2f5a37b7dfded839af97c2dcfddfeb7bf
221RDB148 <125148699+221RDB148@users.noreply.github.com>
2023-03-04 17:00:04 +0000

    dunno

---
+++ tests.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+
+list = [9,7,5,5,2,9,9,9,2,-1]
+
+parents = [-1]
+def func(n,parents):
+    for i,j in enumerate(list):
+        if ()
--- tree_height.py
+++ tree_height.py
@@ -2,7 +2,7 @@
 
 import sys
 import threading
-import numpy
+import numpy as np
 
 
 def compute_height(n, parents):
@@ -14,14 +14,55 @@ def compute_height(n, parents):
 
 def main():
     # implement input form keyboard and from files
-    
+
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
-    
+
     # input number of elements
+    # e = input()
+    e = 5
+
     # input values in one variable, separate with space, split these values in an array
+    # v = input()
+    v = "4 -1 4 1 1"
+    list = []
+    number = ""
+    for i,j in enumerate(v):
+        number = number + j
+        if(j == ' '):
+            list.append(int(number))
+            number = ""
+            continue
+        if(len(v)-1 == i):
+            list.append(int(number))
+    print(list)
+
+    l = []
+
+    for i,j in enumerate(list):
+        if j == -1:
+            l.append(i)
+    for i,j in enumerate(list):
+        if j == l[0]:
+            l.append(i)
+    print("l: ", l)
+
+
+    new_list = []
+    for i,j in enumerate(l):
+        if i == 0:
+            continue
+        print(j)
+        new_list.append(list[list==j])
+    print("new_list", new_list)
+
+
+
+            
+        
+    
+    
     # call the function and output it's result
-    pass
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
@@ -29,5 +70,5 @@ def main():
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
-main()
+# main()
 # print(numpy.array([1,2,3]))
