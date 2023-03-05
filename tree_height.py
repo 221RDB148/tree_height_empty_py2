f70b415e7f15de8997a123d4f774c6818af5cb3c
221RDB148 <125148699+221RDB148@users.noreply.github.com>
2023-03-05 23:01:29 +0000

    updated inputs

--- tree_height.py
+++ tree_height.py
@@ -30,44 +30,36 @@ def compute_height(a,list,l,ss):
 def main():
 # implement input form keyboard and from files
     
-# let user input file name to use, don't allow file names with letter a
-# account for github input inprecision
+    # let user input file name to use, don't allow file names with letter a
+    # account for github input inprecision
+    # input number of elements
     # i = input()
-    inp = "I"
+    inp = input()
+    if inp[0] == "I":
+        e = input()
+        v = input()
+        # v = "4 -1 4 1 1"
+        list = []
+        
+        # makes list from input
+        number = ""
+        for i,j in enumerate(v):
+            number = number + j
+            if(j == ' '):
+                list.append(int(number))
+                number = ""
+                continue
+            if(len(v)-1 == i):
+                list.append(int(number))
 
-# input number of elements
-    e = 5
-    e = input()
-    # e = input()
-    # e = 5
+        # finds first value
+        for i,j in enumerate(list):
+            if j == -1:
+                a = i
 
-# input values in one variable, separate with space, split these values in an array
-    # v = input()
-    # v = input()
-    v = "4 -1 4 1 1"
-    list = []
-    
-   
-# makes list from input
-    number = ""
-    for i,j in enumerate(v):
-        number = number + j
-        if(j == ' '):
-            list.append(int(number))
-            number = ""
-            continue
-        if(len(v)-1 == i):
-            list.append(int(number))
-
-# finds first value
-    for i,j in enumerate(list):
-        if j == -1:
-            a = i
-
-    l = []
-    ss = [0]
-
-    compute_height(a,list,l,ss)
+        l = []
+        ss = [0]
+        compute_height(a,list,l,ss)
 
 sys.setrecursionlimit(10**7)
 threading.stack_size(2**27)
